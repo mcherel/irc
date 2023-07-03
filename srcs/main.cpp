2ca88ab19a1e86046f5e937382b06bc22bbc6adf
Maria <mcherel-@student.42.fr>
2023-07-03 16:20:10 +0200

    server program taking two parameters : port and any password and making server listening on that port and making communicate multiple clients that will connect to the same port as a server

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,67 +10,214 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <iostream> // Include the necessary header file for cout
+#include <cstring> // Include the necessary header file for strerror
+#include <cerrno> // Include the necessary header file for errno
 #include <stdlib.h>
 #include <stdexcept>
+//#include <stdio.h>
 
 //SOCKET RELATED LIB
-#include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#define PORT 8080
+#include <netinet/in.h> // Include the necessary header file for sockaddr_in and sockaddr_in6
+#include <string>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <netdb.h> // Include the necessary header file for getaddrinfo, freeaddrinfo, gai_strerror
+#include <sys/types.h> // Include the necessary header file for socket, setsockopt, bind, listen
+#include <sys/socket.h> // Include the necessary header file for socket, setsockopt, bind, listen
+#include <unistd.h> // Include the necessary header file for close
 
+// Get sockaddr, IPv4 or IPv6:
+void* get_in_addr(struct sockaddr* sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(reinterpret_cast<struct sockaddr_in*>(sa)->sin_addr);
+    }
+
+    return &(reinterpret_cast<struct sockaddr_in6*>(sa)->sin6_addr);
+}
+
+int get_listener_socket(const char* port)
+{
+    int listener; // Listening socket descriptor
+    int yes = 1; // For setsockopt() SO_REUSEADDR, below
+    int rv;
+
+    struct addrinfo hints, *ai, *p;
+
+    // Get us a socket and bind it
+    memset(&hints, 0, sizeof(hints)); // Clear the hints structure
+    hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP socket
+    hints.ai_flags = AI_PASSIVE; // For wildcard IP address
+    if ((rv = getaddrinfo(NULL, port, &hints, &ai)) != 0) {
+        std::cerr << "selectserver: " << gai_strerror(rv) << std::endl; // Print the error message
+        std::exit(1); // Exit the program with an error status
+    }
+
+    for (p = ai; p != NULL; p = p->ai_next) {
+        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol); // Create the socket
+        if (listener < 0) {
+            continue;
+        }
+
+        // Lose the pesky "address already in use" error message
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, (char*)&yes, sizeof(int)); // Set the socket option
+
+        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) { // Bind the socket
+            close(listener);
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(ai); // All done with this
+
+    // If we got here, it means we didn't get bound
+    if (p == NULL) {
+        return -1;
+    }
+
+    // Listen
+    if (listen(listener, 10) == -1) { // Start listening on the socket
+        return -1;
+    }
+
+    return listener; // Return the listening socket descriptor
+}
+
+
+void add_to_pfds(pollfd* pfds[], int newfd, int* fd_count, int* fd_size)
+{
+    // If we don't have room, add more space in the pfds array
+    if (*fd_count == *fd_size) {
+        *fd_size *= 2; // Double it
+
+        // Reallocate memory for the pfds array, using reinterpret_cast for the type cast
+        *pfds = reinterpret_cast<pollfd*>(realloc(*pfds, sizeof(**pfds) * (*fd_size)));
+    }
+
+    // Assign the new file descriptor and events to the pfds array
+    (*pfds)[*fd_count].fd = newfd;
+    (*pfds)[*fd_count].events = POLLIN; // Check ready-to-read
+
+    // Increment the fd_count
+    (*fd_count)++;
+}
+
+// Remove an index from the set
+void del_from_pfds(struct pollfd pfds[], int i, int *fd_count)
+{
+    // Copy the one from the end over this one
+    pfds[i] = pfds[*fd_count-1];
+
+    (*fd_count)--;
+}
 
 int main (int argc, char  const**argv){
-    try{
-        (void)argv;
-        //std::cout << argc << std::endl;
-        if (argc != 3)
+    if (argc != 3)
             throw std::logic_error("The program call : ./ircserv <port> <password>");
-        int server_fd, new_socket, valread;
-        //(void)new_socket;
-        //(void)valread;
-        struct sockaddr_in address;
-        int opt = 1;
-        int addrlen = sizeof(address);
-        char buffer[1024] = {0};
-        std::string hello = "Hello from server";
-        
-        //Socket FD creation
-        if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-            throw std::logic_error("socket failed");
+    int listener;     // Listening socket descriptor
 
-        //Attaching socket to the port 8080
-        if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR \
-            | SO_REUSEPORT, &opt, sizeof(opt))){
-                throw std::logic_error("setsockopt");
-            }
-        address.sin_family = AF_INET;//IPv4
-        address.sin_addr.s_addr = INADDR_ANY;//Binds Socket to all available interfaces
-        address.sin_port = htons(PORT);
+    int newfd;        // Newly accept()ed socket descriptor
+    struct sockaddr_storage remoteaddr; // Client address
+    socklen_t addrlen;
 
-        if (bind(server_fd, (struct  sockaddr*)&address, sizeof(address)) < 0)
-            throw std::logic_error("bind failed");
-        
-        if (listen(server_fd, 3) < 0)
-            throw std::logic_error("listen");
+    char buf[256];    // Buffer for client data
 
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)
-            throw std::logic_error("accept");
-        
-        valread = read(new_socket, buffer, 1024);
-        std::cout << buffer << std::endl;
-        send(new_socket, hello.c_str(), hello.size(), 0);
-        std::cout << "Hello message sent" << std::endl;
+    char remoteIP[INET6_ADDRSTRLEN];
 
-        //https://www.geeksforgeeks.org/socket-programming-cc/
+    // Start off with room for 5 connections
+    // (We'll realloc as necessary)
+    int fd_count = 0;
+    int fd_size = 5;
+    const char *port = argv[1];
+    struct pollfd *pfds = static_cast<pollfd*>(malloc(sizeof *pfds * fd_size));
+
+    // Set up and get a listening socket
+    listener = get_listener_socket(port);
+
+    if (listener == -1) {
+        std::cerr << "error getting listening socket" << std::endl;
+        std::exit(1);
     }
 
-    catch( const std::exception & e ) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+    // Add the listener to set
+    pfds[0].fd = listener;
+    pfds[0].events = POLLIN; // Report ready to read on incoming connection
+
+    fd_count = 1; // For the listener
+
+    // Main loop
+    for(;;) {
+        int poll_count = poll(pfds, fd_count, -1);
+
+        if (poll_count == -1) {
+            std::cerr << "error poll" << std::endl;
+            exit(1);
+        }
+
+        // Run through the existing connections looking for data to read
+        for(int i = 0; i < fd_count; i++) {
+
+            // Check if someone's ready to read
+            if (pfds[i].revents & POLLIN) { // We got one!!
+                if (pfds[i].fd == listener) {
+                    // If listener is ready to read, handle new connection
+
+                    addrlen = sizeof(remoteaddr);
+                    newfd = accept(listener, reinterpret_cast<struct sockaddr*>(&remoteaddr), reinterpret_cast<socklen_t*>(&addrlen));
+
+                    if (newfd == -1) {
+                        std::cerr << "error accept" << std::endl;
+                    } else {
+                        add_to_pfds(&pfds, newfd, &fd_count, &fd_size);
+                        inet_ntop(remoteaddr.ss_family,
+                                get_in_addr(reinterpret_cast<struct sockaddr*>(&remoteaddr)),
+                                remoteIP, INET6_ADDRSTRLEN);
+
+                        std::cout << "pollserver: new connection from " <<
+                            remoteIP << " on socket " << newfd << std::endl;
+                    }
+                }else {
+                    // If not the listener, we're just a regular client
+                    int nbytes = recv(pfds[i].fd, buf, sizeof buf, 0);
+
+                    int sender_fd = pfds[i].fd;
+
+                    if (nbytes <= 0) {
+                        // Got error or connection closed by client
+                        if (nbytes == 0) {
+                            // Connection closed
+                            std::cout << "pollserver: socket " << sender_fd << " hung up" << std::endl;
+                        } else {
+                            std::cerr << ("recv") << std::endl;
+}
+
+                        close(pfds[i].fd); // Bye!
+
+                        del_from_pfds(pfds, i, &fd_count);
+
+                    } else {
+                        // We got some good data from a client
+
+                        for(int j = 0; j < fd_count; j++) {
+                            // Send to everyone!
+                            int dest_fd = pfds[j].fd;
+
+                           // Except the listener and ourselves
+                            if (dest_fd != listener && dest_fd != sender_fd) {
+                                if (send(dest_fd, buf, nbytes, 0) == -1) {
+                                    std::cerr << "send: " << strerror(errno) << std::endl;
+                                }
+                            }
+                        }
+                    }
+                } // END handle data from client
+            } // END got ready-to-read from poll()
+        } // END looping through file descriptors
+    } // END for(;;)--and you thought it would never end!
+    
     return EXIT_SUCCESS;
 }
